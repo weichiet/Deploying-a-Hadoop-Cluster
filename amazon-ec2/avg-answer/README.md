7a85efb02587490d2b3e694ff3b0c32246b5064c
Wei Chiet <weichiet@outlook.com>
2017-09-07 14:32:20 +0800

    feat: Add MapReduce program and README for Q2 of Lesson 1

---
+++ amazon-ec2/avg-answer/README.md
@@ -0,0 +1,17 @@
+## Average number of answers
+Write a mapper and reducer that calculates the average number of answers for each tag. The output should look like:
+
+``` python
+tag1 2.23
+tag2 1.76
+tag3 0.1
+...
+```
+## Question
+Q: What's the tag with the highest average number of answers?  
+A: 'home-office' is the tags with the highest average number of answers, with 9 answers on average.
+
+## Files
+`mapper.py`: mapper of this problem  
+`reducer.py`: reducer of this problem  
+`part-00000`: output of the MapReduce program
---
+++ amazon-ec2/avg-answer/reducer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import sys
+
+def reducer():
+
+    tag_count = 0
+    answer_count = 0
+    old_tag = None
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        if len(data) != 2:
+            continue
+
+        this_tag, count = data
+
+        if old_tag and old_tag != this_tag:
+            
+            print("{}\t{:.3f}".format(old_tag, answer_count/tag_count))
+
+            tag_count = 0
+            answer_count = 0
+            
+        old_tag = this_tag
+        tag_count += 1
+        
+        if old_tag and old_tag == this_tag:
+            answer_count += int(count)
+        
+    if old_tag and old_tag == this_tag:
+        answer_count += int(count)        
+        print("{}\t{:.3f}".format(old_tag, answer_count/tag_count))       
+
+if __name__ == "__main__":
+    reducer()
